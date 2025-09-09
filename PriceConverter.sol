e37047ebd5975f3f6d4a6c91df019c0afb91ee09
Amitya31 <amitya1576@gmail.com>
2025-09-09 22:16:29 +0530

    sending an eth

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
+// library can add different functionalities to contract ; bu tyou cant create a state variable and you cant send ether
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; //importing from npm  
 
 library PriceConverter {
-    function getPrice() public view returns(uint256){
+    function getPrice() internal view returns(uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (,int256 answer,,,) = priceFeed.latestRoundData(); // its like destructuring in javascript
         // returns price of Eth in terms of USd
         return uint256(answer * 1e10); // as msg.value is of 10^18 while answer is of only 10^8 eth
     }
-    function getConversionRate(uint256 ethAmount) external view returns(uint256) {
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
         uint256 ethPrice = getPrice(); // current eth Price means USD
         uint256 ethAmountToUsd = (ethPrice * ethAmount) / 1e18; //current ethPrice * input eth Amount
         return ethAmountToUsd;
--- fundme.sol
+++ fundme.sol
@@ -17,5 +17,32 @@ contract FundMe{
         addresstoAmount[msg.sender] = addresstoAmount[msg.sender] + msg.value; // the amount the sender sends multiple times
     }
 
+    function withdraw() public {
+        for(/*starting index, ending index, step index */ uint256 funderIndex = 0; funderIndex < funder.length; funderIndex++){
+            address funders = funder[funderIndex];
+            addresstoAmount[funders] = 0;
+        }
+        funder = new address[](0); //resetting the array with type address to 0
+        //sending an Eth from a contract
+        //There are three ways to actually send eth from a contract
+
+        //transfer
+        //send
+        //call
+
+        //msg.sender = type address
+        //payable(msg.sender) = type payable address
+        //in solidity to send the native blockvhain token such as ethereum it can be only done by payable
+        // lets start with the transfer so to transfer the funds whoever is calling the widthdraw function
+        payable(msg.sender).transfer(address(this).balance); //this keyword refers to this wholw contract 
+        //address(this).balance //with this we get native currency of this address using this
+        // as we know that when you transfer fund from one account to another it cost gas fees which is around 2100 hence "transfer" is capped at 2300 means if more than this gas is used it will give an error
+        // as we know that when you transfer fund from one account to another it cost gas fees which is around 2100 hence "send" is capped at 2300 means if more than this gas is used it will give boolean as return means if txn completed than 
+
+        //send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        
+
+    }
     
 }
